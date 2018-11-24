c5694f11b87320c0b8b3c92c1fc0473c045f689d
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-11-24 13:15:25 -0700

    Modal Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
 $(document).on("click", ".webButton", function(){
-    console.log("working")
+    let id = $(this).attr("id");
+    let description = $(this).attr("data-info");
+    let site = $(this).attr("data-site");
+    console.log(id);
+    console.log(description);
+    $("#exampleModalCenterTitle").text(id);
+    $(".modal-body").text(description);
+    $("#siteButton").attr("data-site", site);
+});
+
+$(document).on("click", "#siteButton", function(){
+    let web = $(this).attr("data-site");
+    console.log(web);
+    window.open(web, "_blank")
 })
