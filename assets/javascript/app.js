fcfa5964aa202160c5c17a76bac4643d4ee644d8
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-12-01 12:50:23 -0700

    .gifs updated and fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).on("click", ".webButton", function(){
+$(document).on("click", ".webButton", function () {
     let id = $(this).attr("id");
     let description = $(this).attr("data-info");
     let site = $(this).attr("data-site");
@@ -9,8 +9,20 @@ $(document).on("click", ".webButton", function(){
     $("#siteButton").attr("data-site", site);
 });
 
-$(document).on("click", "#siteButton", function(){
+$(document).on("click", "#siteButton", function () {
     let web = $(this).attr("data-site");
     console.log(web);
     window.open(web, "_blank")
 })
+
+$(".webButton").on("mouseenter", function () {
+    console.log("in")
+    $(this).css("background-image", "url(" + $(this).attr("data-animated") + ")");
+
+})
+$(".webButton").on("mouseleave", function () {
+    console.log("out")
+    $(this).css("background-image", "url(" + $(this).attr("data-still") + ")");
+
+
+});
